fe5efa397cb9575e999e9e2791f1cb0e78d96ffa
Sümeyye KAYA <sumeyye.kaya@baogrenci.com>
2019-10-24 14:14:07 +0300

    dosya işlendi

---
+++ KararYapilari/Form2.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace KararYapilari
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnSatıs = new System.Windows.Forms.Button();
+            this.btnMarketKontrol = new System.Windows.Forms.Button();
+            this.btnNotKontrol = new System.Windows.Forms.Button();
+            this.btnGirisKontrol = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnGiris2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 8);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 9;
+            // 
+            // btnSatıs
+            // 
+            this.btnSatıs.Location = new System.Drawing.Point(12, 176);
+            this.btnSatıs.Name = "btnSatıs";
+            this.btnSatıs.Size = new System.Drawing.Size(100, 23);
+            this.btnSatıs.TabIndex = 8;
+            this.btnSatıs.Text = "Satış İşlemleri";
+            this.btnSatıs.UseVisualStyleBackColor = true;
+            this.btnSatıs.Click += new System.EventHandler(this.BtnSatıs_Click);
+            // 
+            // btnMarketKontrol
+            // 
+            this.btnMarketKontrol.Location = new System.Drawing.Point(12, 147);
+            this.btnMarketKontrol.Name = "btnMarketKontrol";
+            this.btnMarketKontrol.Size = new System.Drawing.Size(100, 23);
+            this.btnMarketKontrol.TabIndex = 7;
+            this.btnMarketKontrol.Text = "Market Kontrolü";
+            this.btnMarketKontrol.UseVisualStyleBackColor = true;
+            this.btnMarketKontrol.Click += new System.EventHandler(this.BtnMarketKontrol_Click);
+            // 
+            // btnNotKontrol
+            // 
+            this.btnNotKontrol.Location = new System.Drawing.Point(12, 118);
+            this.btnNotKontrol.Name = "btnNotKontrol";
+            this.btnNotKontrol.Size = new System.Drawing.Size(100, 23);
+            this.btnNotKontrol.TabIndex = 6;
+            this.btnNotKontrol.Text = "Not Kontrolü";
+            this.btnNotKontrol.UseVisualStyleBackColor = true;
+            this.btnNotKontrol.Click += new System.EventHandler(this.BtnNotKontrol_Click);
+            // 
+            // btnGirisKontrol
+            // 
+            this.btnGirisKontrol.Location = new System.Drawing.Point(12, 60);
+            this.btnGirisKontrol.Name = "btnGirisKontrol";
+            this.btnGirisKontrol.Size = new System.Drawing.Size(100, 23);
+            this.btnGirisKontrol.TabIndex = 5;
+            this.btnGirisKontrol.Text = "Giriş Bilgisi Kontrolü";
+            this.btnGirisKontrol.UseVisualStyleBackColor = true;
+            this.btnGirisKontrol.Click += new System.EventHandler(this.BtnGirisKontrol_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 34);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 10;
+            this.textBox2.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
+            // 
+            // btnGiris2
+            // 
+            this.btnGiris2.Location = new System.Drawing.Point(12, 89);
+            this.btnGiris2.Name = "btnGiris2";
+            this.btnGiris2.Size = new System.Drawing.Size(100, 23);
+            this.btnGiris2.TabIndex = 11;
+            this.btnGiris2.Text = "Giriş Bilgisi Kontrolü";
+            this.btnGiris2.UseVisualStyleBackColor = true;
+            this.btnGiris2.Click += new System.EventHandler(this.BtnGiris2_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(127, 211);
+            this.Controls.Add(this.btnGiris2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnSatıs);
+            this.Controls.Add(this.btnMarketKontrol);
+            this.Controls.Add(this.btnNotKontrol);
+            this.Controls.Add(this.btnGirisKontrol);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnSatıs;
+        private System.Windows.Forms.Button btnMarketKontrol;
+        private System.Windows.Forms.Button btnNotKontrol;
+        private System.Windows.Forms.Button btnGirisKontrol;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnGiris2;
+    }
+}
---
+++ KararYapilari/Form2.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KararYapilari
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnGirisKontrol_Click(object sender, EventArgs e)
+        {
+            string username = "admin";
+            string password = "123";
+            string result = "";
+
+            if (username == textBox1.Text)
+            {
+                if (password == textBox2.Text)
+                {
+                    result = "Tebrikler!";
+                }
+                else
+                {
+                    result = "Şifreniz Yanlış.\nAnne sütü önemli.";
+                }
+            }
+            else
+            {
+                result = "Kullanıcı adın yanlış.\nYine hayal kırıklığısın.";
+            }
+            MessageBox.Show(result);
+        }
+
+        private void BtnNotKontrol_Click(object sender, EventArgs e)
+        {
+            //Girilen not 0-30 ise "FF" 30-50 "DD" 50-70 "BB" 70-100 "AA"
+
+            int not = int.Parse(textBox1.Text);
+            string result = "Harf Notunuz: ";
+
+            if (not >= 0 && not <= 30)
+                result += "FF"; //eğer kod tek satırsa süslü parantez olmasa da olur
+            else if (not > 30 && not <= 50)
+            {
+                result += "DD"; // + işareti yukarıdaki result metnine ak metin yapmanı sağlıyo
+            }
+            else if (not > 50 && not <= 70)
+            {
+                result += "BB";
+            }
+            else if (not > 70 && not <= 100)
+            {
+                result += "AA";
+            }
+            else
+            {
+                result = "Geçerli bir aralık giriniz.";
+            }
+            MessageBox.Show(result);
+
+
+            //KISA YOL:
+            /*
+            if (not >= 0 && not <= 100)
+            {
+                if (not <= 30) { result += "FF"; }
+                else if (not <= 50) { result += "DD"; }
+                else if (not <= 70) { result += "CC"; }
+                else if (not <= 85) { result += "BB"; }
+                else { result += "AA"; }
+            }
+            else
+                result = "Geçerli bir aralık giriniz.";
+            MessageBox.Show(result);
+            */
+        }
+
+        private void BtnMarketKontrol_Click(object sender, EventArgs e)
+        {
+            /*Dışarıdan ürün adı girilecek, kasiyer bize ürünün hangi reyonda olduğunu söyleyecek.
+             Domates, Biber, Patlıcan => Sebze Reyonu
+             Diş Macunu, Parfüm, Şampuan => Kozmetik R
+             Cep Telefonu, Bilgisayar, Ses Sistemi => Teknoloji R
+             Başka bir ürün girildiğinde "Bu ürün elimizde bulunmamaktadır."*/
+
+            string ürün = textBox1.Text.ToLower(); // hepsini küçük harfe çevirir yazılanların
+            string m = "";
+            if (ürün == "domates" || ürün == "biber" || ürün == "patlıcan")
+                m = "Sebze Reyonu";
+            else if (ürün == "diş macunu" || ürün == "parfüm" || ürün == "şampuan")
+                m = "Kozmetik Reyonu";
+            else if (ürün == "cep Telefonu" || ürün == "bilgisayar" || ürün == "ses sistemi")
+                m = "Teknoloji Reyonu";
+            else
+                m = "Bu ürün elimizde bulunmamaktadır.";
+            MessageBox.Show(m);
+        }
+
+        private void BtnSatıs_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void BtnGiris2_Click(object sender, EventArgs e)
+        {
+            string username = "admin";
+            string password = "123";
+            string result = "";
+
+            if(textBox1.Text == username && textBox2.Text == password)
+            {
+                result = "Tebriks kanki :)";
+            }
+            else
+            {
+                result = "Davar.";
+            }
+            MessageBox.Show(result);
+        }
+    }
+}
